cb2806a6802fc655b04b9bd804195646d3fc07f4
De4dSh0t <ruipsilva2001@hotmail.com>
2020-09-25 09:28:17 +0100

    GenericSingleton v1.0

---
+++ GenericSingleton/GenericSingleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GenericSingleton/GenericSingleton/Singleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericSingleton
+{
+    public class Singleton<T> where T : new()
+    {
+        private static readonly T InstanceHolder = new T();
+        
+        static Singleton() {}
+
+        private Singleton() {}
+
+        public static T Instance => InstanceHolder;
+    }
+}
